cb17fafef1ef3028849f763b889be542a620dce8
Salejo-ap <salejoap1202@gmail.com>
2026-07-30 11:40:13 -0500

    calculo de asimetria y courtosis

--- Semana_vector_medias_matriz_varianza_covarianza_correclacion.r
+++ Semana_vector_medias_matriz_varianza_covarianza_correclacion.r
@@ -15,3 +15,19 @@ matriz_covarianza
 ## calcular la matriz de correlacion
 matriz_correlacion <- cor(matriz)
 matriz_correlacion
+# instalar paquete moments pra calcualar la curtosis y asimetria
+install.packages("moments")
+require(moments)
+## calcular asiemtria
+asimetria <- apply(matriz, 2, skewness)
+## calcular curtosis
+curtosis <- apply(matriz, 2, kurtosis)
+## asimetría de la distribución de los datos respecto a la media muestral:
+### sk(xk) > 0: distribución asimétrica positiva o a derecha.
+### sk(xk) < 0: distribución asimétrica negativa o a izquierda.
+asimetria
+## Describe el comportamiento en las colas de la distribución de los datos: 
+###Datos de una distribución normal: k(xk) ≈ 3.
+###k(xk) > 3: distribución leptocúrtica.
+###k(xk) < 3: distribución platicúrtica.
+curtosis
