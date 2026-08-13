aaac8d2ba7ee7d138c46ffc61d1282bdf558d97c
Salejo-ap <salejoap1202@gmail.com>
2026-08-13 11:49:45 -0500

    Se agreagn las presentaciones de la semana 3 y se cargan los datos para el ejercicio de normalidad univariada y pruebas de normalidad

--- Distribucion_normal_multivariada.r
+++ Distribucion_normal_multivariada.r
@@ -6,11 +6,11 @@ library(mvtnorm)
 
 ### Paremetros
 
-n<-100
+n <- 100
 
 #### vector de medias
 
-mu<-c(70,170)
+mu <- c(70, 170)
 
 #### matriz de varianza y covarianza
 
@@ -19,20 +19,29 @@ mu <- c(70 , 170)
 rho <- 0.8
 s11 <- 100
 s22 <- 64
-s12 <- rho * sqrt ( s11 ) * sqrt ( s22 )
-Sigma <- matrix (c( s11 , s12 , s12 , s22 ) , nrow = 2, byrow = TRUE )
-sd_1 <- sqrt ( s11 ) ; sd_2 <- sqrt ( s22 )
+s12 <- rho * sqrt(s11) * sqrt(s22)
+sigma <- matrix(c(s11, s12, s12, s22), nrow = 2, byrow = TRUE)
+sd_1 <- sqrt(s11)
+sd_2 <- sqrt(s22)
 
 
 ### Malla de puntos
 
-Peso <- seq ( mu [1] - 3*sd_1, mu [1] + 3*sd_1, length = n )
-Altura <- seq( mu [2] - 3*sd_2, mu [2] + 3*sd_2 , length = n)
+peso <- seq(mu [1] - 3 * sd_1, mu [1] + 3 * sd_1, length = n)
+altura <- seq(mu [2] - 3 * sd_2, mu [2] + 3 * sd_2, length = n)
 
 ### obtener la densidad de probabilidad
 
 set.seed(123)
-Densidad <- function (x1 , x2 )
-    dmvnorm ( cbind ( x1 , x2 ) , mu , Sigma )
-f <- outer ( Peso , Altura , FUN = Densidad)
-f
+densidad <- function(x1, x2){
+    dmvnorm(cbind(x1, x2), mu, sigma)
+}
+f <- outer(peso, altura, FUN = "densidad")
+
+persp(peso, altura, f,
+    theta = 20 , phi = 30 ,
+    xlab = " Peso ", ylab = " Altura ", zlab = "f(Peso , Edad ) ",
+    zlim = c(0 , 0.005) ,
+    ticktype = " detailed ", nticks = 6,
+    col = "# FFE1FF ")
+title (main = bquote (" D i s t r i b u c i n Normal Bivariada (" ~ rho == .( rho ) ~ ")"))
---
+++ Prueba_de_normalidad_univariada_transformaciones.r
@@ -0,0 +1,4 @@
+# Prueba de normalidad
+## importacion de los datos
+data<- read.table("DataExample_4_14.txt", header=TRUE)
+## Grafico dsitribucion univariada de cada variable
