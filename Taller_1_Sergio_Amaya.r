90973460c95d84816299173eb2c9f4e64fe79865
Salejo-ap <salejoap1202@gmail.com>
2026-08-18 11:27:49 -0500

    Se guardan los archivos para el desarrollo del taller 1

---
+++ Taller_1_Sergio_Amaya.r
@@ -0,0 +1,17 @@
+# Sergio Alejandro Amaya Paez ---- Taller 1 ----18/08/2026
+# 0.1. Análisis descriptivo
+##A. ¿Cuál es el contenido mineral promedio de los dos lados de estos tres huesos? ¿Cuál es su covarianza? Interprete
+##¿La acumulación de mineral en las estructuras óseas ocurre de manera homogénea? (10 puntos).
+##B. Existe correlación entre las mediciones de los dos lados de estos tres huesos considerando todas las variables
+##vs todas? De qué magnitud es? Interprete. (10 puntos)
+##C. Interprete el siguiente gráfico multivariado
+# 0.2. Normalidad Multivariante
+## A. Grafique la distribución univariada de cada variable, y evalue la normalidad univariada usando esos gráficos 
+## y una prueba de hipótesis de cada variable. Concluya.
+## B. Dibuje las elipeses entre cada posible par de variables. ¿Qué puede decir sobre la
+## distribución normal bivariada de esos pares de variables?.
+## C. Calcule las distancias de Mahalanobis, grafique el qqplot e interprete ¿Hay sospecha de normalidad multivariada?
+## D. Identifique si hay observacines atípicas. Si las hay, indique cuáles son y responda qué haría con ellas ¿Tendrán 
+## efecto sobre la normalidad multivariada?
+## E. Elija una prueba de normalidad multivariada (justificando su elección) y úsela para concluir si los datos 
+## siguen una distribución normal multivariada.
